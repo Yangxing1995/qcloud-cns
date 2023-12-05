522bf455066b6cd55009199de8018e4e82965f06
jace.yang <jace.yang@trustasia.com>
2023-12-05 15:44:24 +0800

    feat(request): 支持设置全局的请求地址

--- request.go
+++ request.go
@@ -10,19 +10,19 @@ import (
 	"time"
 )
 
-//API响应的接口
+// API响应的接口
 type Responser interface {
 	Error() error
 }
 
-//基础的API响应结构
+// 基础的API响应结构
 type BaseResponse struct {
 	Code     int
 	Message  string
 	CodeDesc string
 }
 
-//API响应中识别并构造API错误的方法
+// API响应中识别并构造API错误的方法
 func (resp BaseResponse) Error() error {
 	if resp.Code == 0 {
 		return nil
@@ -31,15 +31,43 @@ func (resp BaseResponse) Error() error {
 	return fmt.Errorf("[%d](%s): %s", resp.Code, resp.CodeDesc, resp.Message)
 }
 
-//云解析API请求的资源地址
-const Uri = "cns.api.qcloud.com/v2/index.php"
+// 云解析API请求的资源地址
+var (
+	_host   = "cns.api.qcloud.com"
+	_uri    = _host + "/v2/index.php"
+	_scheme = "https"
+)
 
-//GET类型的API请求封装
+func GetHost() string {
+	return _host
+}
+
+// SetHost 设置API请求的域名
+func SetHost(str string) {
+	_host = str
+	_uri = _host + "/v2/index.php"
+}
+
+// SetHost 设置API请求的地址
+func SetUri(str string) {
+	_uri = str
+}
+
+// SetScheme 设置API请求的协议
+func SetScheme(str string) {
+	_scheme = str
+}
+
+func buildUri() string {
+	return _scheme + "://" + _uri
+}
+
+// GET类型的API请求封装
 func (cli *Client) requestGET(action string, param url.Values, respInfo interface{}) error {
 	return cli.request("GET", action, param, nil, respInfo)
 }
 
-//API请求的封装（内建公共参数、签名的设置）
+// API请求的封装（内建公共参数、签名的设置）
 func (cli *Client) request(method, action string, param url.Values, body io.Reader, respInfo interface{}) error {
 	if param == nil {
 		param = url.Values{}
@@ -51,10 +79,10 @@ func (cli *Client) request(method, action string, param url.Values, body io.Read
 	param.Set("Nonce", "123456")
 	param.Set("SecretId", cli.SecretId)
 
-	sig := Signature(param, method, Uri, cli.SecretKey)
+	sig := Signature(param, method, _uri, cli.SecretKey)
 	param.Set("Signature", sig)
 
-	req, err := http.NewRequest(method, "https://"+Uri+"?"+param.Encode(), body)
+	req, err := http.NewRequest(method, buildUri()+"?"+param.Encode(), body)
 	if err != nil {
 		return fmt.Errorf("构建请求错误: %s", err)
 	}
